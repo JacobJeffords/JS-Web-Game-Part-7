d7d02fe44f9aebfd080bc6b92c8080dbbe144e96
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-08-11 23:36:58 -0400

    First and final commit

--- index.js
+++ index.js
@@ -2,10 +2,39 @@
 const pc = newPlayableCharacter(100, 110)
 
 // Create a non-playable character
-const npc = newNonPlayableCharacter(50, 300)
+const npc1 = newNonPlayableCharacter(10, 300)
+const npc2 = newNonPlayableCharacter(110, 300)
+const npc3 = newNonPlayableCharacter(210, 300)
+const npc4 = newNonPlayableCharacter(310, 300)
+const npc5 = newNonPlayableCharacter(410, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+async function moveNPC(npc) {
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
+
+async function infiniteLoopWalk(npc) {
+    await npc.walkEast(500);
+    await npc.walkNorth(500);
+    await npc.walkWest(500);
+    await npc.walkSouth(500);
+    infiniteLoopWalk(npc);
+}
+
+infiniteLoopWalk(npc1);
+
+// moveNPC(npc1);
+// moveNPC(npc2);
+// moveNPC(npc3);
+// moveNPC(npc4);
+// moveNPC(npc5);
 
 // Create the inventory
 const inventory = newInventory()
